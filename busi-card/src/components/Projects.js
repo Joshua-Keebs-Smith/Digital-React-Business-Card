7ec5ee8b15a1fc1a6df3a49cbb3a994adf062214
Joshua-Keebs-Smith <90350464+Joshua-Keebs-Smith@users.noreply.github.com>
2022-05-16 09:22:00 -0600

    images

--- busi-card/src/components/Projects.js
+++ busi-card/src/components/Projects.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Projects() {
     return (
-        <footer>
+        <div>
             <p>Future Projects Here.</p>
-        </footer>
+        </div>
     )
 }
